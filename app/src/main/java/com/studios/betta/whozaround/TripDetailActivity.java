691a3bc4fd39a1544ca0351cdc5775976615df02
martincrb <mcristobal@cs.upc.edu>
2016-04-21 13:51:59 +0200

    Detail Trip screen

--- app/src/main/java/com/studios/betta/whozaround/TripDetailActivity.java
+++ app/src/main/java/com/studios/betta/whozaround/TripDetailActivity.java
@@ -1,14 +1,22 @@
 package com.studios.betta.whozaround;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Window;
 
 public class TripDetailActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        getActionBar().setTitle("");
+        getActionBar().setElevation(0);
         setContentView(R.layout.activity_trip_detail);
+
         getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
